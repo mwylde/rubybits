c95bebc05e6626cc279278b8e5a925d53741b2db
Micah Wylde <mwylde@wesleyan.edu>
2010-11-21 22:43:59 -0500

    Now generates appropriate checksum

--- lib/rubybits.rb
+++ lib/rubybits.rb
@@ -10,18 +10,24 @@ module RubyBits
 				end
 			}
 			
-			def fields; @_fields; end
+			def checksum field, &block
+				@_checksum_field = [field, block]
+			end
 			
+			def fields; @_fields; end
+			def checksum_field; @_checksum_field; end
+
 			private
 			def field kind, name, size, description, options
 				@_fields ||= []
 				@_fields << [kind, name, size, description, options]
 				self.class_eval %{
 					def #{name}= val
-						@_#{name} = val
+						@__#{name} = val
+						@_checksum_cached = false
 					end
 					def #{name}
-						@_#{name}
+						@__#{name}
 					end
 				}
 			end
@@ -31,41 +37,16 @@ module RubyBits
 			values.each{|key, value|
 				self.send "#{key}=", value
 			}
+			@_checksum_cached = false
 		end
-		
+				
 		# Returns a binary string representation of the structure according to the fields defined
 		# and their current values.
 		# @return [String] bit string representing struct
-		def to_s_old
-			offset = 0
-			buffer = []
-			# This method works by iterating through each bit of each field and setting the bits in
-			# the current output byte appropriately.
-			self.class.fields.collect{|field|
-				kind, name, size, description, options = field
-				data = self.send(name)
-				data ||= 0
-				bytes = [data].pack(PACK_MAP[kind]).bytes.to_a
-				b_iter = (size/8.0).ceil
-				size.times do |bit|
-					buffer << 0 if offset % 8 == 0
-					b_iter -= 1 if bit % 8 == 0
-					# the largest multiple of 8 smaller than size
-					lm_of_8 = (size/8)*8
-					# we need to calculate this function:
-					# f(x) = {8 if bit < lm_of_8; size-lm_of_8 otherwise}
-					buffer[-1] |= (get_bit(bytes[b_iter], (bit < lm_of_8 ? 8 : size-lm_of_8)-(bit % 8)-1) << 7-(offset % 8))
-					offset += 1
-				end
-			}
-			puts
-			puts buffer.collect{|x| "%08b" % x}.inspect
-			puts [0b11010001, 0b10101010, 0b11111010, 0b10001110].collect{|x| "%08b" % x}.inspect
-			
-			buffer.pack("c*")
-		end
-		
-		def to_s
+		def to_s(do_checksum = true)
+			if self.class.checksum_field && do_checksum && !@_checksum_cached
+				self.calculate_checksum
+			end
 			offset = 0
 			buffer = []
 			# This method works by iterating through each bit of each field and setting the bits in
@@ -86,14 +67,25 @@ module RubyBits
 			#puts [0b11010001, 0b10101010, 0b11111010, 0b10001110].collect{|x| "%08b" % x}.inspect
 			
 			buffer.pack("c*")
+			
 		end
 		
 		
-		private
+		protected
+		
 		PACK_MAP = {
 			:unsigned => "I",
 			:signed   => "i"
 		}
+		
+		def calculate_checksum
+			if self.class.checksum_field
+				self.send("#{self.class.checksum_field[0]}=", 0)
+				checksum = self.class.checksum_field[1].call(self.to_s(false).bytes.to_a)
+				self.send("#{self.class.checksum_field[0]}=", checksum)
+				@_checksum_cached = true
+			end
+		end
 		
 		def set_bit(byte, bit, value)
 			#TODO: this can probably be made more efficient
--- spec/rubybits_spec.rb
+++ spec/rubybits_spec.rb
@@ -63,5 +63,33 @@ describe "Structure" do
 		tf = TestFormat5.new(:field1 => 0b11010, :field2 => 0b001, :field3 => 0b101010, :field4 => 0b1011, :field5 => 0b11101010001, :field6 => 0b11)
 		tf.to_s.bytes.to_a.should == [0b11010001, 0b10101010, 0b11111010, 0b10001110]
 	end
+	
+	it "should allow signed integers" do
+		class TestFormat6 < RubyBits::Structure
+			signed :field1, 8, "Field1"
+			signed :field2, 4, "Field2"
+			signed :field3, 4, "Field3"
+		end
+		
+		tf = TestFormat6.new(:field1 => -10, :field2 => -4, :field3 => -8)
+		tf.to_s.bytes.to_a.should == [0b11110110, 0b11001000]
+	end
+	
+	it "should calculate checksum correctly" do
+		class TestFormat7 < RubyBits::Structure
+			unsigned :field1,  8,  "Field1"
+			unsigned :field2,  4,  "Field2"
+			unsigned :field3,  4,  "Flag"
+			unsigned :field4,  16, "Field3"
+			unsigned :checksum, 8, "Checksum (sum of all previous fields)"
+			
+			checksum :checksum do |bytes|
+				bytes[0..-2].inject{|sum, byte| sum += byte} & 255
+			end
+		end
+		
+		tf = TestFormat7.new(:field1 => 0x77, :field2 => 0x06, :field3 => 0x0F, :field4 => 0x726b)
+		tf.to_s.bytes.to_a.should == "work".bytes.to_a << ((0x77 + 0x6F + 0x72 + 0x6B) & 255)
+	end
 end
 
